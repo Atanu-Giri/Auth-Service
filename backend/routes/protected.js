be3f89eea7e0a88726868bd54c93e35963c9877f
Atanu Giri <atanugiri75@gmail.com>
2026-04-16 02:44:03 +0530

    dashboard & frontend

---
+++ backend/routes/protected.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import authMiddleware from '../middlewares/authMiddleware.js';
+
+const router = express.Router();
+
+router.get('/dashboard', authMiddleware, (req, res) => {
+  res.json({ message: `Welcome to your dashboard`,
+    user: req.user
+   });
+});
+
+export default router;
